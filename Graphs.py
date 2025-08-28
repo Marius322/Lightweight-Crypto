b08c3972d9f8867210062e0e4012b0d441897cee
Marius322 <Marius.Furtig-Rytterager.2023@mumail.ie>
2025-08-28 11:52:31 +0100

    sperated Xn vs Xn+1 graphs

--- Graphs.py
+++ Graphs.py
@@ -9,7 +9,7 @@ Contans graphing functions for;
 
 import numpy as np
 import matplotlib.pyplot as plt
-from Digital_Logistic_Map import digital_map
+from Digital_Map import digital_map
 
 # Logistic Map
 
@@ -45,7 +45,55 @@ def Logistic_map(x, n):
 
 # Graphing Xn vs Xn+1 for Logistic and Digital Map 
 
-def Graph_Map(x, n, k, save = False):
+def graph_logistic_map(x, n, save = False):
+    '''
+
+    Parameters
+    ----------
+    x : Seed/Initial Input
+    
+    n : Number of iterations
+    
+    save : When true, the resulting graph is saved to a svg file.
+           Defaults to being false
+
+    Returns
+    -------
+    Graph of xn vs xn+1 for Logistic Map
+
+    '''
+    
+    # Generating Sequence of Random Numbers for each Map
+    x_vals = Logistic_map(x, n)  
+    
+    # Generating Sequence without last entry
+    x_n = x_vals[:-1] 
+    
+    # Generating Sequence without last entry
+    x_np1 = x_vals[1:] 
+
+    # Plotting Graph  
+    
+    fig, _ = plt.subplots(figsize=(6, 6))
+    plt.plot(x_n, x_np1, 'bo', label = 'Logistic Map')
+    plt.legend()
+    plt.grid()
+    plt.xlabel('Xn')
+    plt.ylabel('Xn+1')
+    plt.title('Plot of Next State vs Current State for the Logistic Map')
+    
+    # Add Description
+    fig.subplots_adjust(bottom=0.15)
+    fig.text(0.5, 0.05, f"Shows the dynamics of the Logistic Map with r = 4 and x = {x}",
+         ha='center', fontsize=10)
+    
+    # Save Plot if Enabled
+    if save == True:
+        plt.savefig(f"Logistic_Map_xn_vs_xn+1_{x}_{n}.svg") 
+        
+    plt.show() 
+
+def graph_digital_map(x, n, k, save = False):
     '''
 
     Parameters
@@ -56,40 +104,98 @@ def Graph_Map(x, n, k, save = False):
     
     k : Number of digits in each binary number
     
-    save : When true, the resulting graph is saved to a png file.
+    save : When true, the resulting graph is saved to a svg file.
            Defaults to being false
 
     Returns
     -------
-    Graph of xn vs xn+1 for Logistic and Digital Map
+    Graph of xn vs xn+1 for Digital Map
 
     '''
     
-    # Generating Sequence of Random Numbers for each Map
-    x_vals = Logistic_map(x, n)  
+    # Generating Sequence of Random Numbers for each Map 
     x_vals_digital, _ = digital_map(x, n, k) 
     
     # Generating Sequence without last entry
-    x_n = x_vals[:-1] 
     x_n_digital = x_vals_digital[:-1] 
     
     # Generating Sequence without last entry
-    x_np1 = x_vals[1:] 
     x_np1_digital = x_vals_digital[1:] 
 
     # Plotting Graph  
-    plt.plot(x_n, x_np1, 'ro', label = 'logistic map')
-    plt.plot(x_n_digital, x_np1_digital, 'go', label = 'digital map')
+    
+    fig, _ = plt.subplots(figsize=(6, 6))
+    plt.plot(x_n_digital, x_np1_digital, 'go', label = 'Digital Map')
     plt.legend()
     plt.grid()
     plt.xlabel('Xn')
     plt.ylabel('Xn+1')
-    plt.title('Digital Logistic Map - Xn+1 vs Xn')
+    plt.title('Plot of Next State vs Current State for the Digital Map')
+        
+    # Add Description
+    fig.subplots_adjust(bottom=0.15)
+    fig.text(0.5, 0.05, f"Shows the dynamics of the Digital Map with r = 4, x = {x} and k = {k}",
+         ha='center', fontsize=10)
     
+    # Save Plot if Enabled
     if save == True:
-        plt.savefig(f"Digital Logistic Map - Xn+1 vs Xn - {x}_{n}.png") 
+        plt.savefig(f"Digital_Map_xn_vs_xn+1_{x}_{n}_{k}.svg") 
         
     plt.show() 
+
+def graph_all_maps(x, n, k, save = False):
+    '''
+
+    Parameters
+    ----------
+    x : Seed/Initial Input
+    
+    n : Number of iterations
+    
+    k : Number of digits in each binary number
+    
+    save : When true, the resulting graph is saved to a svg file.
+           Defaults to being false
+
+    Returns
+    -------
+    xn vs xn+1 for Digital and Logistic Map on same graph
+
+    '''
+    
+    # Generating Sequence of Random Numbers for each Map 
+    x_vals_digital, _ = digital_map(x, n, k) 
+    x_vals = Logistic_map(x, n)  
+    
+    # Generating Sequence without last entry
+    x_n_digital = x_vals_digital[:-1] 
+    x_n = x_vals[:-1] 
+    
+    # Generating Sequence without last entry
+    x_np1_digital = x_vals_digital[1:] 
+    x_np1 = x_vals[1:] 
+
+    # Plotting Graph  
+    
+    fig, _ = plt.subplots(figsize=(6, 6))
+    plt.plot(x_n_digital, x_np1_digital, 'go', label = 'Digital Map')
+    plt.plot(x_n, x_np1, 'bo', label = 'Logistic Map')
+    plt.legend()
+    plt.grid()
+    plt.xlabel('Xn')
+    plt.ylabel('Xn+1')
+    plt.title('Plot of Next State vs Current State for the Logistic Map')
+        
+    # Add Description
+    fig.subplots_adjust(bottom=0.15)
+    fig.text(0.5, 0.05, f"Shows the dynamics of the Logistic and Digital Map with r = 4, x = {x} and k = {k}",
+         ha='center', fontsize=10)
+    
+    # Save Plot if enabled
+    if save == True:
+        plt.savefig(f"Digital_and_Logistic_Map_xn_vs_xn+1_{x}_{n}_{k}.svg") 
+        
+    plt.show()    
 
 # Helper Functions
 
@@ -139,7 +245,7 @@ def cobweb_plot_logistic(x0, n, save = False):
         
     n : Number of iterations
     
-    save : When true, the resulting graph is saved to a png file. 
+    save : When true, the resulting graph is saved to a svg file. 
            Defaults to being false
 
     Returns
@@ -151,7 +257,7 @@ def cobweb_plot_logistic(x0, n, save = False):
     x = np.linspace(0.001, 0.999, 500)
     fx_total = np.array([one_step_map_logistic(xi) for xi in x])
 
-    plt.figure(figsize=(8, 8))
+    fig, _ = plt.subplots(figsize=(6, 6))
     plt.plot(x, fx_total, label='f(x)') # Graph of Logistic Map
     plt.plot(x, x, 'k--', label='y = x')  # Diagonal Line
 
@@ -170,16 +276,23 @@ def cobweb_plot_logistic(x0, n, save = False):
         # Draws horizontal line from (x, f(x)) to (f(x), f(x))
         x_vals.append(fx) 
         fx_vals.append(fx)
-
-    plt.plot(x_vals, fx_vals, color='red', lw=1, label='Cobweb Path')
+    
+    # Graph Cobweb Plot
+    plt.plot(x_vals, fx_vals, color='blue', lw=1, label='Cobweb Path')
     plt.title('Cobweb Plot of Logistic Map')
     plt.xlabel('$x_n$')
     plt.ylabel('$x_{n+1}$')
     plt.legend()
     plt.grid(True)
     
+    # Add Description
+    fig.subplots_adjust(bottom=0.15)
+    fig.text(0.5, 0.05, f"Displays the cobweb plot of the Logistic Map with r = 4 and x = {x0}",
+         ha='center', fontsize=10)
+    
+    # Save Plot if enabled
     if save == True:
-        plt.savefig(f"Logistic Cobweb Plot of {x0}_{n}.png") 
+        plt.savefig(f"Logistic_Cobweb_Plot_of_{x0}_{n}.svg") 
         
     plt.show()
 
@@ -194,7 +307,7 @@ def cobweb_plot_digital(x0, n, k, save = False):
     
     k : Number of digits in each binary number
     
-    save : When true, the resulting graph is saved to a png file. 
+    save : When true, the resulting graph is saved to a svg file. 
            Defaults to being false 
 
     Returns
@@ -206,7 +319,7 @@ def cobweb_plot_digital(x0, n, k, save = False):
     x = np.linspace(0.001, 0.999, 500)
     fx_total = np.array([one_step_map_digital(xi, k) for xi in x])
 
-    plt.figure(figsize=(8, 8))
+    fig, _ = plt.subplots(figsize=(6, 6))
     plt.plot(x, fx_total, label='f(x)') # Graph of Digital Map
     plt.plot(x, x, 'k--', label='y = x')  # Diagonal Line
 
@@ -225,16 +338,31 @@ def cobweb_plot_digital(x0, n, k, save = False):
         # Draws horizontal line from (x, f(x)) to (f(x), f(x))
         x_vals.append(fx) 
         fx_vals.append(fx)
-
-    plt.plot(x_vals, fx_vals, color='red', lw=1, label='Cobweb Path')
+    
+    # Graph Cobweb Plot
+    plt.plot(x_vals, fx_vals, color='green', lw=1, label='Cobweb Path')
     plt.title('Cobweb Plot of Digital Map')
     plt.xlabel('$x_n$')
     plt.ylabel('$x_{n+1}$')
     plt.legend()
     plt.grid(True)
     
+    # Add Description
+    fig.subplots_adjust(bottom=0.15)
+    fig.text(0.5, 0.05, f"Displays the cobweb plot of the Digital Map with r = 4, x = {x0} and k = {k}",
+         ha='center', fontsize=10)
+    
+    # Save Plot if enabled
     if save == True:
-        plt.savefig(f"Digital Cobweb Plot of {x0}_{n}.png") 
+        plt.savefig(f"Digital_Cobweb_Plot_of_{x0}_{n}.svg") 
         
     plt.show()
 
+X = np.array([0.324, 0.617, 0.888])
+k = 64
+
+for i in range(len(X)):
+    cobweb_plot_logistic(x0 = X[i], n = 100, save = True)
+    cobweb_plot_logistic(x0 = X[i], n = 1000, save = True)
+    cobweb_plot_digital(x0 = X[i], n = 100, k = k, save = True)
+    cobweb_plot_digital(x0 = X[i], n = 1000, k = k, save = True)
